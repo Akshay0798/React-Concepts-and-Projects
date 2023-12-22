092f9d733fc8f0ad749bdf4f97369f77a88b5d11
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-22 15:16:28 +0530

    add btnText by props

--- telwind-props/src/Component/Card.jsx
+++ telwind-props/src/Component/Card.jsx
@@ -1,4 +1,4 @@
-function Card({ username }) {
+function Card({ username,btnText }) {
   // console.log(props);
   // console.log(props.username);
   console.log(username);
@@ -12,13 +12,13 @@ function Card({ username }) {
       />
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
-        <h1 className="text-lg font-semibold text-white">{username}</h1> 
+        <h1 className="text-lg font-semibold text-white">{username}</h1>
         <p className="mt-2 text-sm text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
           debitis?
         </p>
         <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-          View Profile &rarr;
+          {btnText || "Fuck You"} &rarr;
         </button>
       </div>
     </div>

126467b25200595dd9b54c7c219d458a1ffac3c6
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-23 18:31:53 +0530

    update range

--- 03Password-generator/src/App.jsx
+++ 03Password-generator/src/App.jsx
@@ -1,43 +1,56 @@
+// Importing necessary hooks and styles
 import { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
 
+// Main component function
 function App() {
-  const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  // State variables using the useState hook
+  const [length, setLength] = useState(8); // Password length
+  const [numberAllowed, setNumberAllowed] = useState(false); // Whether numbers are allowed
+  const [charAllowed, setCharAllowed] = useState(false); // Whether special characters are allowed
+  const [password, setPassword] = useState(""); // Generated password
 
-  // useRef hook
+  // useRef hook to store a reference to the password input field
   const passwordRef = useRef(null);
 
+  // Function to generate a random password based on options
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    // Include numbers if allowed
     if (numberAllowed) str += "0123456789";
+    // Include special characters if allowed
     if (charAllowed) str += "!@#$%^&*-_+=[]{}";
 
+    // Generate password of specified length
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
 
+    // Set the generated password to the state
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  // Function to copy the generated password to the clipboard
   const copyPasswordToClipboard = useCallback(() => {
+    // Select and copy the password using the clipboard API
     passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 999);
+    passwordRef.current?.setSelectionRange(0, 100);
     window.navigator.clipboard.writeText(password);
   }, [password]);
 
+  // useEffect hook to regenerate the password when options change
   useEffect(() => {
     passwordGenerator();
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
+  // JSX rendering of the component
   return (
     <div className="container">
       <h1 className="title">Password generator</h1>
       <div className="password-container">
+        {/* Input field for displaying the generated password */}
         <input
           type="text"
           value={password}
@@ -46,11 +59,14 @@ function App() {
           readOnly
           ref={passwordRef}
         />
+        {/* Button to copy the password to clipboard */}
         <button onClick={copyPasswordToClipboard} className="copy-button">
           Copy
         </button>
       </div>
+      {/* Options for customizing the generated password */}
       <div className="options-container">
+        {/* Option for setting the password length */}
         <div className="option">
           <input
             type="range"
@@ -64,6 +80,7 @@ function App() {
           />
           <label>Length: {length}</label>
         </div>
+        {/* Option for including numbers in the password */}
         <div className="option">
           <input
             type="checkbox"
@@ -75,6 +92,7 @@ function App() {
           />
           <label htmlFor="numberInput">Numbers</label>
         </div>
+        {/* Option for including special characters in the password */}
         <div className="option">
           <input
             type="checkbox"
@@ -91,4 +109,5 @@ function App() {
   );
 }
 
+// Exporting the App component as the default export
 export default App;
